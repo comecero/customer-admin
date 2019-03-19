5f646239ebfad0d58a147b5c4e63d631f2349da6
david <david@SACAGAWEA>
2019-03-19 16:33:51 -0600

    Bug fix invoice link

--- app/pages/invoices/pay_invoice/script.js
+++ app/pages/invoices/pay_invoice/script.js
@@ -3,17 +3,11 @@
     // Get the token
     var token = localStorage.getItem("token");
 
-    // Define the host
-    var host = "api.comecero.com";
-    if (window.location.hostname.indexOf("-staging.") > -1) {
-        host = "api-staging.comecero.com";
-    }
-
     // Get the query parameters
     var params = utils.getPageQueryParameters();
 
     // Define the URL
-    var url = "https://" + host + "/api/v1/invoices/" + params["invoice_id"] + "/links";
+    var url = "/api/v1/invoices/" + params["invoice_id"] + "/links";
 
     // Make a request to get the notification body
     if (params["invoice_id"]) {
